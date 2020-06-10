a3121afb1aaba3d24edc33b41d0f8a849394e04e
aeonincode <khashayar.sasannia@hotmail.com>
2020-06-10 16:22:52 +0200

    update form

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
+import Input from './common/input';
 
 class LoginForm extends Component {
+  state = {
+    account: { username: '', password: '' },
+    errors: {},
+  };
+
+  validate = () => {
+    return { username: 'Username is required.' };
+  };
+
+  //username = React.createRef();
+  //   componentDidMount() {
+  //     this.username.current.focus();
+  //   }
+
   handleSubmit = (e) => {
     e.preventDefault();
 
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
+
     // Call the server
+    //const username = this.username.current.value;
+  };
+
+  handleChange = ({ currentTarget: input }) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
   };
 
   render() {
+    const { account } = this.state;
+
     return (
       <div>
         <hi>Login</hi>
         <form onSubmit={this.handleSubmit}>
-          <div className='form-group'>
-            <label htmlFor='username'>Username</label>
-            <input id='username' type='text' className='form-control' />
-          </div>
-          <div className='form-group'>
-            <label htmlFor='password'>Password</label>
-            <input id='password' type='text' className='form-control' />
-          </div>
+          <Input
+            name='username'
+            value={account.username}
+            label='Username'
+            onChange={this.handleChange}
+          />
+          <Input
+            name='password'
+            value={account.password}
+            label='Password'
+            onChange={this.handleChange}
+          />
           <button className='btn btn-primary'>Login</button>
         </form>
       </div>
