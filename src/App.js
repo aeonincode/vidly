c46b5c5a996122a0336ba411f4f39af6b62b4bc7
aeonincode <khashayar.sasannia@hotmail.com>
2020-06-15 12:02:25 +0200

    end form section

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 import './App.css';
 
 class App extends Component {
@@ -29,6 +30,7 @@ class App extends Component {
         <NavBar />
         <main className='container'>
           <Switch>
+            <Route path='/register' component={RegisterForm} />
             <Route path='/login' component={LoginForm} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies} />
